54e3a555f5cb8e05c1e231b1c3366e010e517295
Shubham Kashyap <shubham.kashyap.jha07@gmail.com>
2024-09-13 22:01:06 +0530

    first commit

---
+++ learn-spring-boot/learn-spring-boot/src/main/java/com/in28minutes/springboot/learnspringboot/LearnSpringBootApplication.java
@@ -0,0 +1,16 @@
+package com.in28minutes.springboot.learnspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+
+@SpringBootApplication
+@Configuration
+public class LearnSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBootApplication.class, args);
+	}
+
+}
